72e8388897242c7d54b3a36956b43649deb7891b
Roger Deetz <rdeetz@gmail.com>
2024-01-28 11:05:18 -0500

    First file picker.

--- QUnoFluent/MainWindow.xaml.cs
+++ QUnoFluent/MainWindow.xaml.cs
@@ -5,6 +5,9 @@
 namespace Mooville.QUno.Fluent
 {
     using Microsoft.UI.Xaml;
+    using System;
+    using Windows.Storage.Pickers;
+    using WinRT.Interop;
 
     public sealed partial class MainWindow : Window
     {
@@ -12,6 +15,33 @@ namespace Mooville.QUno.Fluent
         {
             this.InitializeComponent();
             this.ExtendsContentIntoTitleBar = true;
+
+            this.buttonOpen.Click += this.ButtonOpen_Click;
+        }
+
+        private async void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var fileOpenPicker = new FileOpenPicker();
+
+            var hWnd = WindowNative.GetWindowHandle(this);
+            InitializeWithWindow.Initialize(fileOpenPicker, hWnd);
+
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+            fileOpenPicker.FileTypeFilter.Add(@".quno");
+
+            var file = await fileOpenPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                textFile.Text = file.Path;
+            }
+            else
+            {
+                textFile.Text = "Operation cancelled.";
+            }
+
+            return;
         }
     }
 }
